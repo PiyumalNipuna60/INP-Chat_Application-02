bef590b868d86f3fb68d5f23b4372f738809db25
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-15 01:39:29 +0530

    Assets

--- My-chat_application/src/controller/ServerApp.java
+++ My-chat_application/src/controller/ServerApp.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ServerApp {
+    public TextArea txtAreaMsg;
+    public TextField txtMsg;
+    public Button btnSent;
+
+    public void btnSentOnAction(ActionEvent actionEvent) {
+    }
 }
