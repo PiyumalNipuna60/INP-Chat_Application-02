b8cd019be793982a62b49b0c09769a9290dd1b91
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-15 01:45:55 +0530

    Modify Server UI

--- My-chat_application/src/controller/ServerApp.java
+++ My-chat_application/src/controller/ServerApp.java
@@ -4,12 +4,20 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class ServerApp {
     public TextArea txtAreaMsg;
     public TextField txtMsg;
     public Button btnSent;
+    
 
-    public void btnSentOnAction(ActionEvent actionEvent) {
+    public void sentImageOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void sentStickerOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void sent_massageOnMouseClicked(MouseEvent mouseEvent) {
     }
 }
