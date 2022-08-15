ec14bb0494ab8f979d4685bec0eb70e3ef5baa85
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-15 10:44:45 +0530

    Add ClientAppController

---
+++ My-chat_application/src/controller/ClientAppController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ClientAppController {
+    public Label txtClientName;
+    public TextArea txtAreaMsg;
+    public TextField txtMsg;
+
+    public void sentImageOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void sentStickerOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void sent_massageOnMouseClicked(MouseEvent mouseEvent) {
+    }
+}
