310ccf9f3f1c935265021a8bd3ebd6654ad39854
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-15 10:50:05 +0530

    Rename ServerAppController

--- My-chat_application/src/controller/ServerAppController.java
+++ My-chat_application/src/controller/ServerAppController.java
@@ -1,16 +1,14 @@
 package controller;
 
-import javafx.event.ActionEvent;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
-public class ServerApp {
+public class ServerAppController {
     public TextArea txtAreaMsg;
     public TextField txtMsg;
-    public Button btnSent;
-    
 
     public void sentImageOnMouseClicked(MouseEvent mouseEvent) {
     }
