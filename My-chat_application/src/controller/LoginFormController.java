4807a92be21fbf861d3a9f735144105c43026d89
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-15 10:34:41 +0530

    fix loginForm

--- My-chat_application/src/controller/LoginFormController.java
+++ My-chat_application/src/controller/LoginFormController.java
@@ -1,8 +1,13 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class LoginFormController {
+    public Button btnLogIn;
+    public TextField txtUserName;
+
     public void btnLogInOnAction(ActionEvent actionEvent) {
     }
 }
