a4eab5fb0f920fa482428115fbde1e40c39ccadd
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-15 11:36:22 +0530

    Add LoginForm

--- My-chat_application/src/controller/LoginFormController.java
+++ My-chat_application/src/controller/LoginFormController.java
@@ -1,13 +1,31 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LoginFormController {
     public Button btnLogIn;
     public TextField txtUserName;
 
-    public void btnLogInOnAction(ActionEvent actionEvent) {
+    public ArrayList<String> Users= new ArrayList<String>();
+
+    public void btnLogInOnAction(ActionEvent actionEvent) throws IOException {
+        String userName=txtUserName.getText();
+        System.out.println(Users);
+        if(Users.contains(userName)){
+            System.out.println("already");
+        }else {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientApp.fxml"))));
+            stage.show();
+        }
+        Users.add(userName);
     }
 }
