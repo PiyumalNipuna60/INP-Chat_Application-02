2b3df4f879172a18eaa8ae8439653f0a678269d6
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-15 11:27:29 +0530

    Add ClientAppController content

--- My-chat_application/src/controller/ClientAppController.java
+++ My-chat_application/src/controller/ClientAppController.java
@@ -5,10 +5,20 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ClientAppController {
     public Label txtClientName;
     public TextArea txtAreaMsg;
     public TextField txtMsg;
+    final int PORT=9005;
+    Socket socket;
+    DataOutputStream dataOutputStream;
+    DataInputStream dataInputStream;
+
+    String massage = "", reply = "";
 
     public void sentImageOnMouseClicked(MouseEvent mouseEvent) {
     }
