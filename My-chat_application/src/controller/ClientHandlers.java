e79bdc5a76e06e30d638715a8a8f8636924f0db7
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-18 22:09:35 +0530

    Add ClientHandler class: set Constructoe value

--- My-chat_application/src/controller/ClientHandlers.java
+++ My-chat_application/src/controller/ClientHandlers.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -11,8 +13,14 @@ public class ClientHandlers extends Thread{
     public BufferedReader in;
     public PrintWriter writer;
 
-    public ClientHandlers(Socket socket,ArrayList<ClientHandlers> clients) {
+    public ClientHandlers(Socket socket,ArrayList<ClientHandlers> clients) throws IOException {
         this.clients = clients;
         this.socket = socket;
+        this.in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer=new PrintWriter(socket.getOutputStream());
     }
+
+
+
+
 }
