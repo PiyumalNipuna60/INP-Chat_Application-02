c7f132ade61c288d0cdbdde0059ea7542d4502ba
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-18 22:06:49 +0530

    Add ClientHandler class: variable

--- My-chat_application/src/controller/ClientHandlers.java
+++ My-chat_application/src/controller/ClientHandlers.java
@@ -1,4 +1,18 @@
 package controller;
 
-public class ClientHandlers {
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandlers extends Thread{
+    private ArrayList<ClientHandlers> clients;
+    private Socket socket;
+    public BufferedReader in;
+    public PrintWriter writer;
+
+    public ClientHandlers(Socket socket,ArrayList<ClientHandlers> clients) {
+        this.clients = clients;
+        this.socket = socket;
+    }
 }
