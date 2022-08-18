b8afe608cb4a775cba0c5d0176226e10f9a7820a
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-18 23:07:47 +0530

    Add ServerInitializer2

---
+++ My-chat_application/src/ServerInitializer2.java
@@ -0,0 +1,31 @@
+
+
+import controller.ClientHandlers;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerInitializer2 {
+    private static ArrayList<ClientHandlers> clients = new ArrayList<ClientHandlers>();
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket;
+        Socket socket;
+        try {
+            serverSocket = new ServerSocket(5002);
+            while (true){
+                System.out.println("Waiting");
+                socket = serverSocket.accept();
+                System.out.println("Connected");
+                System.out.println("-------------------------------------");
+                ClientHandlers thread = new ClientHandlers(socket,clients);
+                clients.add(thread);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
