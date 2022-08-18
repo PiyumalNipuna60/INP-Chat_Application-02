c00b71d7af1e49ee73a2d66952f30e1c2f3a6f95
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-08-18 22:14:44 +0530

    ClientHandler class: set run Method

--- My-chat_application/src/controller/ClientHandlers.java
+++ My-chat_application/src/controller/ClientHandlers.java
@@ -7,20 +7,36 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class ClientHandlers extends Thread{
-    private ArrayList<ClientHandlers> clients;
-    private Socket socket;
+public class ClientHandlers extends Thread {
+    private final ArrayList<ClientHandlers> clients;
+    private final Socket socket;
     public BufferedReader in;
     public PrintWriter writer;
 
-    public ClientHandlers(Socket socket,ArrayList<ClientHandlers> clients) throws IOException {
+    public ClientHandlers(Socket socket, ArrayList<ClientHandlers> clients) throws IOException {
         this.clients = clients;
         this.socket = socket;
-        this.in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        this.writer=new PrintWriter(socket.getOutputStream());
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new PrintWriter(socket.getOutputStream());
     }
 
-
+    @Override
+    public void run() {
+        try {
+            String msg;
+            while ((msg = in.readLine()) != null) {
+                if (msg.equalsIgnoreCase("Exit")) {
+                    break;
+                }
+                for (ClientHandlers cl:clients){
+                    cl.writer.println(msg);
+                    System.out.println(msg);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
